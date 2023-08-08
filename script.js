b22c91da209ba5c34de521f70e82782ef63b5513
Umut Can Tümer <126867824+umutumer@users.noreply.github.com>
2023-08-08 22:36:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const headerList = document.querySelector(".header-list");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    headerList.classList.toggle("active");
+});
+
+document.querySelectorAll(".header-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+   headerList.classList.remove("active");
+}));
+    
+
 //up
 document.addEventListener("DOMContentLoaded", function() {
     const upBtn = document.getElementsByClassName("up")[0];
